7f0d08b9ca9358a432536960af837ed809ee520e
cleverdou <lostaloneesk@126.com>
2020-03-12 17:54:47 +0800

    优化modelDispatch没有缓存的逻辑

--- hooks.js
+++ hooks.js
@@ -4,13 +4,20 @@
 'use strict';
 
 import {useDispatch, useSelector, shallowEqual} from 'react-redux';
+import {useRef} from 'react'
 
-export function useModelDispatch(models) {
+export function useModelDispatch(models={}) {
   const dispatch = useDispatch();
-  return Object.keys(models).reduce((dispatchs, key) => {
-    dispatchs[key] = dispatch[models[key].namespace];
-    return dispatchs;
-  }, {});
+  const prevModels = useRef(null);
+  const result = useRef(null);
+  if (!shallowEqual(prevModels.current, models)) {
+    result.current = Object.keys(models).reduce((dispatchs, key) => {
+      dispatchs[key] = dispatch[models[key].namespace];
+      return dispatchs;
+    }, {});
+  }
+  prevModels.current = models;
+  return result.current;
 }
 
 export function useModelSelector(models, selector, equalityFn) {
